ed6a70ee235040d88c335a5169200a25f4bd6715
Gandhi Mena Salas <gandhimenasalas@macbook-pro-de-gandhi.local>
2016-04-02 15:37:39 -0600

    Initial Commit

---
+++ MyNewPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyNewPlayer
+//
+//  Created by Gandhi Mena Salas on 02/04/16.
+//  Copyright © 2016 Trenx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
