a713b572a882e21b69989e0f37abd7d45df00d29
Gandhi Mena Salas <gandhimenasalas@macbook-pro-de-gandhi.local>
2016-04-08 00:21:42 -0500

    cambié el sender

--- MyNewPlayer/TableViewController.swift
+++ MyNewPlayer/TableViewController.swift
@@ -9,87 +9,57 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-
+    
+    let VC = ViewController()
+    var titleList: [String]!
+    var coverList: [String]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        titleList = VC.titleList
+        coverList = VC.coverList
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+
+        return titleList.count
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        cell.textLabel?.text = titleList[indexPath.row]
+        
+        let coverSong = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(coverList[indexPath.row], ofType: "jpg")!)
+        let coverData = NSData(contentsOfURL: coverSong)
+        cell.imageView?.image = UIImage(data: coverData!)
+        
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        let nextVC: ViewController = segue.destinationViewController as! ViewController
+        let indexPath = self.tableView.indexPathForSelectedRow
+        nextVC.indexSong = indexPath?.row
+        
+        
     }
-    */
+
 
 }
--- MyNewPlayer/ViewController.swift
+++ MyNewPlayer/ViewController.swift
@@ -7,19 +7,163 @@
 //
 
 import UIKit
+import AVFoundation
+import MediaPlayer
 
 class ViewController: UIViewController {
-
+    
+//Variables
+    let prevIcon = UIImage(named: "prev")
+    let playIcon = UIImage(named: "play")
+    let pauseIcon = UIImage(named: "pause")
+    let nextIcon = UIImage(named: "next")
+    
+    //lists
+    let titleList: [String] =   ["title1","title2","title3","title4","title5","title6"]
+    let authorList: [String] =  ["autor1","autor2","autor3","autor4","autor5","autor6"]
+    let songList: [String] =    ["sound1","sound2","sound3","sound4","sound5","sound6"]
+    let coverList: [String] =   ["image1","image2","image3","image4","image5","image6"]
+    
+    //PlayerManager
+    var player = AVAudioPlayer()
+    
+    //index
+    var indexSong:Int!
+    
+//Outlets
+    
+    @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var cover: UIImageView!
+    @IBOutlet weak var titleSong: UILabel!
+    @IBOutlet weak var authorSong: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        prevButton.setImage(prevIcon, forState: .Normal)
+        playButton.setImage(playIcon, forState: .Normal)
+        nextButton.setImage(nextIcon, forState: .Normal)
+        
+        print(indexSong)
+        addResource(songNumber: indexSong!)
+        //45, 510, 230, 20
+        
+        //VolumeControll
+        let formVolume = UIView(frame: CGRectMake(45, 510, 230, 20))
+        let volumeView = MPVolumeView(frame: formVolume.bounds)
+        formVolume.addSubview(volumeView)
+        self.view.addSubview(formVolume)
+    
+    }
+    
+    
+    @IBAction func playerController(sender: UIButton) {
+        
+        switch (sender.titleLabel?.text)! {
+            
+        case "play":
+            
+            if !player.playing{
+                
+                player.play()
+                playButton.setImage(pauseIcon, forState: .Normal)
+                
+            }else{
+                
+                player.pause()
+                playButton.setImage(playIcon, forState: .Normal)
+                
+            }
+            
+        case "prev":
+            
+            if indexSong == 0{
+                indexSong = songList.count
+            }
+            
+            indexSong! -= 1
+            
+            addResource(songNumber: indexSong)
+            print(indexSong)
+            
+            
+        case "next":
+            
+            if indexSong == songList.count - 1{
+                indexSong! = -1
+                
+            }
+            
+            indexSong! += 1
+            
+            addResource(songNumber: indexSong)
+            print(indexSong)
+            
+        default: //this is the shuffleButton
+            
+            let shuffle = Int(arc4random_uniform(UInt32(songList.count)))
+            addResource(songNumber: shuffle)
+            print(shuffle)
+            
+        }
+    }
+    
+    
+    func addResource(songNumber songNumber: Int){
+        
+        let songSource = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(songList[songNumber], ofType: "mp3")!)
+        let coverSource = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(coverList[songNumber], ofType: "jpg")!)
+        let coverData = NSData(contentsOfURL: coverSource)
+        
+        do{
+            
+            player = try AVAudioPlayer(contentsOfURL: songSource)
+            player.play()
+            playButton.setImage(pauseIcon, forState: .Normal)
+        
+            cover.image = UIImage(data: coverData!)
+            titleSong.text = titleList[songNumber]
+            authorSong.text = authorList[songNumber]
+            
+        }catch{
+        
+            print("Error")
+            
+            
+        }
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        
+        if motion == .MotionShake {
+            
+            let shuffle = Int(arc4random_uniform(UInt32(songList.count)))
+            addResource(songNumber: shuffle)
+            print(shuffle)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
+    //**********Volumen con slider
+    /*
+    @IBAction func sliderValueChanged(sender: UISlider) {
+        
+        let selectedValue = Float(sender.value)
+        
+        if reproductor.playing{
+            
+            reproductor.volume = selectedValue
+        }
+     
+    }*/
+    /* //Volumen del dispositivo
+     let wrapperView = UIView(frame: CGRectMake(45, 510, 230, 20))
+     self.view.addSubview(wrapperView)
+     let volumenView = MPVolumeView(frame: wrapperView.bounds)
+     wrapperView.addSubview(volumenView)
+     */
 }
 
